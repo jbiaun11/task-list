9cd64a42bb5c067b90ab62387cf7851e56585128
Julie Biaunie <julie@Julies-MacBook-Pro.local>
2025-05-08 14:12:21 -0400

    setup

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('title', 'List of Tasks')
+
+@section('content')
+        @forelse ($tasks as $task)
+            <div>
+                <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
+            </div>
+        @empty
+            <div>There are no tasks</div>
+        @endforelse
+@endsection
+
